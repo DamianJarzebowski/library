3c6cea7a11b8f5df8769449503e17b20c79fe53c
dmian <damian.r.hajduk@o2.pl>
2021-09-04 14:03:16 +0200

    Initial commit

---
+++ src/Library.java
@@ -0,0 +1,23 @@
+public class Library {
+    public static void main(String[] args) {
+        final String appName = "Biblioteka v0.1";
+
+        Book book1 = new Book();
+        book1.title = "W pustyni i w puszczy";
+        book1.author = "Henryk Sienkiewicz";
+        book1.releaseDate = 2010;
+        book1.pages = 296;
+        book1.publisher = "Greg";
+        book1.isbn = "123456789";
+
+        System.out.println(appName);
+        System.out.println("Książki dostępne w internecie");
+        System.out.println(book1.title);
+        System.out.println(book1.author);
+        System.out.println(book1.releaseDate);
+        System.out.println(book1.pages);
+        System.out.println(book1.publisher);
+        System.out.println(book1.isbn);
+
+    }
+}
